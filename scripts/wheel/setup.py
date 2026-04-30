7a5bb19beba5346979ed6963ffea87179a6791a4
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2026-04-30 21:45:31 +0800

    update

--- scripts/wheel/setup.py
+++ scripts/wheel/setup.py
@@ -33,7 +33,7 @@ elif sys.platform == "linux":
 
 setup(
     name=f"{bindingfor}-ElaWidgetTools",
-    version="0.9.1",
+    version="0.10",
     author="HIllya51",
     license="MIT",
     install_requires=[f"""{bindingfor}"""],
